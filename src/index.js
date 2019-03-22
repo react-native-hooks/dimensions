1fef9a4219822988671a5ca8c700c79f7efb51b9
pritishvaidya <pritishvaidya94@gmail.com>
2019-03-22 17:46:22 +0530

    Add Dimensions hook :collision:

---
+++ src/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from 'react';
+import { Dimensions } from 'react-native';
+
+const enums = ['window', 'screen'];
+
+const initialDimensions = {
+  fontScale: null, height: null, width: null, scale: null,
+};
+
+export default (type) => {
+  if (enums.indexOf(type) === -1) {
+    return initialDimensions;
+  }
+
+  const [dimensions, setDimensions] = useState(initialDimensions);
+
+  useEffect(() => {
+    const currentDimensions = Dimensions.get(type);
+    setDimensions(currentDimensions);
+  }, []);
+
+  useEffect(() => {
+    function dimensionsChange(params) {
+      setDimensions(params[type]);
+    }
+
+    Dimensions.addEventListener('change', dimensionsChange);
+    return () => {
+      Dimensions.removeEventListener('change', dimensionsChange);
+    };
+  }, []);
+
+  return dimensions;
+};
